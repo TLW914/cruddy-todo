80e057671b9f203252917143376d857d8a212d71
TLW914 <leggettwatson.tyler@gmail.com>
2018-12-03 19:59:48 -0800

    implement delete

--- datastore/index.js
+++ datastore/index.js
@@ -83,14 +83,26 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // invoke fs.unlink with filepath w/ id
+    // invoke callback on error
+
+
+    fs.unlink(path.join(exports.dataDir, id +'.txt'),function(err) {
+      if (err) {
+        callback(err);
+      }else {
+        callback();
+      }
+    });
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
